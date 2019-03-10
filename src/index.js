ef24e6979adfe8a9d4e8b678f3982a2ff99c1876
abhay-srivastav <abhay@Administrators-MacBook-Air-4.local>
2019-03-10 18:33:57 +0530

    Push notification

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
-const express = require('express')
-const bodyParser = require('body-parser')
 const axios = require('axios');
-
-const app = express()
-app.listen(8080, () => console.log('Example app listening on port 3000!'))
+let currentInfo = '';
 
 
-app.use(function (err, req, res, next) {
-  res.status(500).send('Something broke!')
-})
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended:true}))
-
-
-app.get('/getCourseInfo',(req,res,next)=>{
-	let courseIds = req.query.courseIds;
-	if(courseIds === undefined){
-		courseIds = '238934';
-	}
+const headers = {
+	'Accept':'application/json',
+	'X-Access-Token':'MTJhMjBlNzdjODFkZjhiYjczNTFhNTBkNjg3YzdhNTYzMjIyMmJjYjZmOTIyNmIxZDhhOWI3Y2Q2YmJmOTI5ZA==',
+	'Cache-Control':'no-cache'
+}
+const pushNotification = ()=>{
+	let courseIds = '2018512,2018498,1866636,1866616,1858526';
 	courseIds = courseIds.split(",");
 	let getCourses = courseIds.map(n=>{
 		const url = `https://www.udemy.com/api-2.0/courses/${Number(n)}?fields[course]=title,num_subscribers,avg_rating,num_reviews`
@@ -29,16 +19,31 @@ app.get('/getCourseInfo',(req,res,next)=>{
 		const response = [];
 		body.forEach(n=>{
 			response.push({"text":n.title,"icon":"i22143"});
-			response.push({"text":String(n.avg_rating),"icon":"i22143"});
-			response.push({"text":String(n.num_reviews),"icon":"i22143"});
-			response.push({"text":String(n.num_subscribers),"icon":"i22143"});
+			response.push({"text":String(n.avg_rating),"icon":"i635"});
+			response.push({"text":String(n.num_reviews),"icon":"i120"});
+			response.push({"text":String(n.num_subscribers),"icon":"i2058"});
 		})
 		body = {
 			"frames":response
 		}
-		res.send(body);
+		let newInfo = JSON.stringify(body);
+		if(newInfo !== currentInfo){
+			currentInfo = newInfo;
+			axios.post('https://developer.lametric.com/api/v1/dev/widget/update/com.lametric.838683764923d84f833723657dfe47a2',body,{headers:headers})
+			.then(()=>{
+				console.log('Notification Sent at '+Date.now());
+			})
+			.catch(()=>{
+				console.log('Error occurred');
+			})
+		}
 	})
 	.catch(err=>{
 		next(err);
 	})
-})
+}
+
+pushNotification();
+setInterval(()=>{
+	pushNotification();
+},60000);
