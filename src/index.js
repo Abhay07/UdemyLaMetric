65c78eca85e307d1373e2db3722db00b19ee37c1
Yogesh <yogesh.yadav@paytm.com>
2019-03-16 19:55:12 +0530

    whitelisting domain

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 
 const app = express()
-const whitelist = ['https://abhay07.github.io', 'http://localhost:8081','http://udemy.abhaysrivastav.net']
+const whitelist = ['https://abhay07.github.io', 'http://localhost:8081','http://udemy.abhaysrivastav.net','http://udemy.music-prod.com']
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
